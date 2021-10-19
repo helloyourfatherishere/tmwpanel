603f2542a578f2d26c71aa8a14c87921ba6ada38
unknown <webworkfirst@gmail.com>
2021-10-19 05:04:00 -0700

    committin

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 var mongoose= require("mongoose");
+var db= "mongodb+srv://ecom:ecom123@db.gby5k.mongodb.net/db?retryWrites=true&w=majority"
 
-mongoose.connect("mongodb://localhost:27017/db",
+mongoose.connect(db,
     {useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
